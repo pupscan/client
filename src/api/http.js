07e6d935b2ab1bba39cbd74a4df06b3b313a3276
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-07-31 08:44:21 +0200

    Add https

--- src/api/http.js
+++ src/api/http.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
 // Configure your axios
-export default axios.create({baseURL: 'http://api.ticket.pupscan.com/'})
+export default axios.create({baseURL: 'https://api.ticket.pupscan.com/'})
